01d7188e6c00fd44007fabafb656a57db0f725f4
Luis Faria <lfariabr@gmail.com>
2024-10-11 03:08:43 -0300

    Database optimization trial

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_login import LoginManager
 from dotenv import load_dotenv
+import logging
 
 app = Flask(__name__, 
             template_folder='../frontend/templates',
@@ -26,7 +27,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # SSL req alre
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# SQLAlchemy optimizations for connection handling
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_pre_ping': True,
+    'pool_size': 5,  # Adjust as per your needs, smaller pools can reduce initialization time.
+    'max_overflow': 10,  # Number of connections exceeding the pool size.
+    'pool_timeout': 30,  # Timeout for getting a connection from the pool.
+    'pool_recycle': 1800  # Recycle connections every 30 minutes.
+}
+
+# db = SQLAlchemy(app)
+db = SQLAlchemy(app, session_options={"autocommit": False, "autoflush": False})
 migrate = Migrate(app, db)
 
 # Flask Login
@@ -38,3 +49,6 @@ login_manager.login_view = 'users.login'
 def load_user(user_id):
     from .users.models import User
     return User.query.get(user_id)
+
+# Adjust logging to reduce verbosity
+logging.basicConfig(level=logging.WARNING)
